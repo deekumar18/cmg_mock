264ca176881d8e35afe6c9754692d6897e15bea2
Kumar <deepak.bh.kumar@accenture.com>
2019-08-22 12:43:53 +0100

    bo classes

---
+++ src/uk/gov/gsi/childmaintenance/www/futurescheme/interfaces/CMECBankWizardWebservice/CMECBankWizardWebservice_BindingImpl.java
@@ -0,0 +1,15 @@
+/**
+ * CMECBankWizardWebservice_BindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package uk.gov.gsi.childmaintenance.www.futurescheme.interfaces.CMECBankWizardWebservice;
+
+public class CMECBankWizardWebservice_BindingImpl implements uk.gov.gsi.childmaintenance.www.futurescheme.interfaces.CMECBankWizardWebservice.CMECBankWizardWebservice_PortType{
+    public uk.gov.gsi.childmaintenance.www.futurescheme.bo.esb.payment.CmecSubmitBankValidationResponse.CmecSubmitBankValidationResponse submitBankValidation(uk.gov.gsi.childmaintenance.www.futurescheme.bo.esb.payment.CmecSubmitBankValidationRequest.CmecSubmitBankValidationRequest bankWizardRequest) throws java.rmi.RemoteException {
+        return null;
+    }
+
+}
